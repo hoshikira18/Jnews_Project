bab5ca2e185ab8b03700b11271c71cda316e8130
Trần Văn Khuyến <khuyendvdtht@gmail.com>
2023-02-07 21:38:20 +0700

    reset repo by Khuyen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import Todolist from './components/Todolist';
 import React from 'react';
 
 function App() {
   return (
-    <div className="bg-gradient-to-b from-green-400 to-blue-500 w-screen h-screen flex flex-col mx-auto items-center justify-center">
-      <Todolist/>
+    <div className="">
+
     </div>
   );
 }
