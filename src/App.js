2ccca554b2f399f9884f5841c71413bef9e67253
Trần Văn Khuyến <khuyendvdtht@gmail.com>
2023-02-07 11:14:44 +0700

    Create Todoapp with handle add task, not remove

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React from 'react';
 
 function App() {
   return (
-    <Todolist/>
+    <div className="bg-gradient-to-b from-green-400 to-blue-500 w-screen h-screen flex flex-col mx-auto items-center justify-center">
+      <Todolist/>
+    </div>
   );
 }
 
--- src/components/Todolist.jsx
+++ src/components/Todolist.jsx
@@ -1,46 +1,35 @@
-import React from "react";
-import { GrAdd } from 'react-icons/gr';
-import {HiOutlineTrash} from 'react-icons/hi'
+import React, { useState } from "react";
+import Task from "./Task";
 
 
 const Todolist = () => {
-     return (
-        <div className="bg-gradient-to-b from-green-400 to-blue-500 max-w-[800px] mt-[96px] w-full h-screen flex flex-col ">
-              <div className="bg-white rounded-md justify-center m-10 p-10 ">
-                <h1 className="font-bold text-black text-3xl p-5">Todo App</h1>
-                <div className="flex flex-row items-center justify-center w-full">
-                    <input className="p-2 border-solid border-black flex rounded-md text-black" type="text" placeholder="Add your new to do" />
-                    <button className="p-2 ml-2 rounded-md bg-[#c559c5]">
-                         <GrAdd size={20} /></button>
-                </div>
-                <div className="w-full bg-gray-200 items-center justify-center text-gray-800 m-3 h-10">
-                    <p className="px-2 pt-2">Buy a new gaming laptop</p>
-                </div>
-                <div className="flex flex-row w-full bg-gray-200 items-center justify-between text-gray-800 m-3 h-10">
-                    <p className="px-2 pt-2 ">Complete a previous task</p>
-                    <button className="bg-[#ff230c]">
-                        <HiOutlineTrash size={40}/>
-                    </button>
-                    
-                </div>
-                <div className="w-full bg-gray-200 items-center justify-center text-gray-800 m-3 h-10">
-                    <p className="px-2 pt-2 ">Create a video for Youtube</p>
-                </div>
-                <div className="w-full bg-gray-200 items-center justify-center text-gray-800 m-3 h-10">
-                    <p className="px-2 pt-2 font-normal">Create a new portfolio site</p>
-                </div>
-                <div className="flex flex-row font-normal items-center justify-between">
-                    <p>You have 4 pending tasks</p>
-                    <button className="bg-[#c559c5] text-white rounded-sm font-normal ml-7 h-8 w-24">Clear All</button>
-                </div>
-                
-                
-                
 
+    const [tasks, setTasks] = useState([
+        "test",
+        "test1"
+    ]);
+    const [input, setInput] = useState('')
+
+    function handleAdd (event) {
+        event.preventDefault();
+        setTasks([...tasks, input]);
+        setInput('');
+        console.log(tasks)
+    }
+
+     return (
+        <form onSubmit={handleAdd} className="w-80 min-h-60 p-4 bg-slate-200 rounded">
+            <div className="input-handle h-12 flex items-center justify-center">
+                <input type="text" value={input} placeholder="Enter a task!" onChange={e => setInput(e.target.value)} className="p-2 rounded outline-none w-[75%]"/>
+                <button type="submit" className="w-[20%] h-[80%] ml-2 bg-slate-500 rounded flex items-center justify-center">ADD</button>
             </div>
-          
-            
-        </div>
+            <div className="tasks p-1">
+                {tasks.map((task, index) => (
+                    <Task key={index} task={task} />
+                ))}
+            </div>
+
+        </form>
     )
 }
 
