a8cf11bd9388c625c168a4fb873c72884b0b14fc
Trần Văn Khuyến <khuyendvdtht@gmail.com>
2023-02-09 16:23:14 +0700

    fix file config tailwindcs by Khuyen

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className="bg-gray-500 w-screen h-screen">
-      
+      test
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,14 @@
 
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
-  theme: {
+  extend: {
+    theme: {
       colors: {
         primary: 'rgb(var(--color-primary) / <alpha-value>)',
         secondary: 'rgb(var(--color-secondary) / <alpha-value>)',
       },
   },
+},
   plugins: [],
   darkMode: "class"
 }
