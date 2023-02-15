d805b5103a3cecef133d1ed54d6b365ac88b66a3
Phong <pp072003@gmail.com>
2023-02-15 18:07:39 +0700

    Phong: test

--- src/App.js
+++ src/App.js
@@ -2,10 +2,26 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="bg-gray-500 w-screen h-screen">
-      test
+    <div className="bg-gray-500 w-screen h-screen ">
+      <Headers />
     </div>
+    
+    
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
