eafff4e7e197ccc9bf2b055630186d278b7bfca7
Phong <pp072003@gmail.com>
2023-02-16 13:56:46 +0700

    Phong : test holdDarkmode

--- src/components/ButtonDarkmode.jsx
+++ src/components/ButtonDarkmode.jsx
@@ -2,21 +2,26 @@
 import React from 'react'
 import { FaMoon, FaSun } from 'react-icons/fa';
 import darkmodeToggle from '../javascripts/darkmode';
+import holdDarkmode from '../javascripts/darkmode';
+import { useEffect } from 'react';
+
 export default function Buttondarkmode() {
+    useEffect(() => {
+        window.onload = holdDarkmode();
+    }, []);
 
-
+    
     return (
 
-        <button className=" flex" onClick={ darkmodeToggle }>
+        <button className=" flex" onClick={darkmodeToggle} >
             <div className='sun'>
                 <FaSun />
             </div>
             <div className='moon hidden text-white'>
                 <FaMoon />
             </div>
-            Test part 22
-
-
         </button>
     )
 }
+
+
--- src/javascripts/darkmode.js
+++ src/javascripts/darkmode.js
@@ -1,16 +1,23 @@
-function darkmodeToggle () {
-    const container = document.querySelector('.container')
-    container.classList.toggle('dark')
-    
-        const sunIcon = document.querySelector(".sun");
-        const moonIcon = document.querySelector(".moon");
-        moonIcon.classList.toggle("hidden");
-        sunIcon.classList.toggle("hidden");
-    
-        
-    
-    
-        
+function darkmodeToggle() {
+  const container = document.querySelector('.container')
+  container.classList.toggle('dark')
+  
+  const sunIcon = document.querySelector(".sun");
+  const moonIcon = document.querySelector(".moon");
+  moonIcon.classList.toggle("hidden");
+  sunIcon.classList.toggle("hidden");
+  const isDarkMode = container.classList.contains('dark');
+  localStorage.setItem('isDarkMode', isDarkMode);
 }
+function holdDarkmode() {
+  const container = document.querySelector('.container')
+  const isDarkMode = container.classList.contains('dark');
 
+  localStorage.setItem('isDarkMode', isDarkMode);
+  
+  const isDark = localStorage.getItem('isDarkMode') === 'true';
+  if (isDark) {
+    container.classList.toggle('dark');
+  }
+}
 export default darkmodeToggle
