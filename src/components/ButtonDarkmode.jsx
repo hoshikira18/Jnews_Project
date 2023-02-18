0e8914c75de7e8daade32955fa76538ffbfb428a
Phong <pp072003@gmail.com>
2023-02-18 22:17:13 +0700

    Phong-darkmode

--- src/components/ButtonDarkmode.jsx
+++ src/components/ButtonDarkmode.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { FaMoon, FaSun } from "react-icons/fa";
 import darkmodeToggle from "../javascripts/darkmode";
+import { holdDarkmode } from "../javascripts/darkmode";
 export default function Buttondarkmode() {
+    useEffect(()=>{
+      window.onload = holdDarkmode();
+    }, []);
+
   return (
     <button
       className="flex text-gray-500 dark:text-gray-400 dark:hover:text-white"
