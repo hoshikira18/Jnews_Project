dd2da4d6be33ba6203f69c80b3ebdb4684aec25c
tranvankhuyen <khuyendvdtht@gmail.com>
2023-02-21 00:21:53 +0700

    fix: CSS (nothing)

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Culture from "./pages/Culture";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="bg-gray-200 w-screen">
+      <div className="bg-gray-200 w-screen rounded">
         <div className="container w-[1252px] bg-gray-300 m-auto">
-          <Header />
+          <Header /> 
           <Routes>
             <Route index element={<Home />} />
             <Route path="/home" element={<Home />} />
