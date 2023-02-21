01a046bfdcf7a62ef8fe27f9a7b4ddd9146b6d41
tranvankhuyen <khuyendvdtht@gmail.com>
2023-02-21 14:36:26 +0700

    query and add data into elements

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,24 +1,28 @@
 import React from "react";
-export default function(){
-    // var a=1;
-    // const num=(a)=> a+1;
-    return(
-        //  <div className="border-t-4 border-sky-500 h-[230px] w-[750px] bg-slate-200 relative"> 
-            <div className=" flex h-[200px] w-full bg-slate-100 py-3 border-t border-slate-300 "> {/* bỏ absolute*/} 
-                <div className="ml-2 w-[250px]">
-                    <img className=" w-full h-full" src="https://kenh14cdn.com/thumb_w/620/203336854389633024/2023/2/18/photo-6-16767070829331501250869.png"></img>
-                </div>
-                <div className="flex flex-col justify-between w-[500px] mx-3">
-                    <h1 className="font-bold text-xl " >Khoe tặng Xemesis “quà khủng” - thứ xin mua 3 tháng mà chưa cho nhưng nhan sắc Xoài Non lại giật hết spotlight</h1>
-                    <div>
-                        <h3 className="inline font-semibold text-sm"> Đời sống - </h3>
-                        <h3 className="inline text-sm text-opacity-75 text-slate-600"> 1 giờ trước </h3>
-                    </div>
-                    <p className=" text-sm ">Mới đây, Xoài Non và Xemesis đã chia sẻ những khoảnh khắc dễ thương và bất ngờ, kỷ niệm 5 năm bên nhau.</p>
-                </div>
-            </div>
-        //  </div>   
-    )
-
-    
+export default function News({ title, topic, image, content, time, id }) {
+  // var a=1;
+  // const num=(a)=> a+1;
+  return (
+    <div className=" flex h-[200px] w-full bg-slate-100 py-3 border-t border-slate-300 ">
+      {" "}
+      {/* bỏ absolute*/}
+      <div className="ml-2 w-[250px]">
+        <img className=" w-full h-full" src={image}></img>
+      </div>
+      <div className="flex flex-col justify-between w-[500px] mx-3">
+        <h1 className="font-bold text-xl">{title}</h1>
+        <div>
+          <h3 className="inline font-semibold text-sm"> {topic} - </h3>
+          <h3 className="inline text-sm text-opacity-75 text-slate-600">
+            {" "}
+            {time}{" "}
+          </h3>
+        </div>
+        <p className=" text-sm ">
+          Mới đây, Xoài Non và Xemesis đã chia sẻ những khoảnh khắc dễ thương và
+          bất ngờ, kỷ niệm 5 năm bên nhau.
+        </p>
+      </div>
+    </div>
+  );
 }
