0c613ec644c816aa61102facb929314960eebe7f
hiep <caoduchiep111@gmail.com>
2023-02-21 17:55:03 +0700

    hiepnews add

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,20 +1,21 @@
 import React from "react";
+import { FaRegFileCode } from "react-icons/fa";
 export default function(){
     // var a=1;
     // const num=(a)=> a+1;
     return(
         //  <div className="border-t-4 border-sky-500 h-[230px] w-[750px] bg-slate-200 relative"> 
-            <div className=" flex h-[200px] w-full bg-slate-100 py-3 border-t border-slate-300 "> {/* bỏ absolute*/} 
+            <div className=" flex h-[200px] w-full bg-slate-100 py-3 border-t border-slate-300 drop-shadow-xl dark:bg-gray-700 dark:border-slate-500" > {/* bỏ absolute*/} 
                 <div className="ml-2 w-[250px]">
                     <img className=" w-full h-full" src="https://kenh14cdn.com/thumb_w/620/203336854389633024/2023/2/18/photo-6-16767070829331501250869.png"></img>
                 </div>
                 <div className="flex flex-col justify-between w-[500px] mx-3">
-                    <h1 className="font-bold text-xl " >Khoe tặng Xemesis “quà khủng” - thứ xin mua 3 tháng mà chưa cho nhưng nhan sắc Xoài Non lại giật hết spotlight</h1>
+                    <h1 className="font-bold text-xl dark:text-slate-100 hover:drop-shadow-2xl" >Khoe tặng Xemesis “quà khủng” - thứ xin mua 3 tháng mà chưa cho nhưng nhan sắc Xoài Non lại giật hết spotlight</h1>
                     <div>
-                        <h3 className="inline font-semibold text-sm"> Đời sống - </h3>
-                        <h3 className="inline text-sm text-opacity-75 text-slate-600"> 1 giờ trước </h3>
+                        <h3 className="inline font-semibold text-sm dark:text-slate-200 "> Đời sống - </h3>
+                        <h3 className="inline text-sm text-opacity-75 text-slate-600 dark:text-slate-300"> 1 giờ trước </h3>
                     </div>
-                    <p className=" text-sm ">Mới đây, Xoài Non và Xemesis đã chia sẻ những khoảnh khắc dễ thương và bất ngờ, kỷ niệm 5 năm bên nhau.</p>
+                    <p className=" text-sm dark:text-slate-300">Mới đây, Xoài Non và Xemesis đã chia sẻ những khoảnh khắc dễ thương và bất ngờ, kỷ niệm 5 năm bên nhau.</p>
                 </div>
             </div>
         //  </div>   
