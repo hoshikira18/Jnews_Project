5058134554d4d62faa48e257b91cc8217fae427b
tranvankhuyen <khuyendvdtht@gmail.com>
2023-02-21 14:29:09 +0700

    query and add data into elements

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,38 @@
 import React from "react";
-import "../App.css";
+import { useState } from "react";
+import { useEffect } from "react";
+import { getPosts } from "../graphql/queries";
 import Scrollbackbutton from "../components/Scrollbackbutton";
 import Footer from "../components/Footer";
-import Post from "../components/Post";
+import News from "../components/News";
 
 function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    getPosts().then((newPosts) => setPosts(newPosts));
+  }, []);
+  console.log(posts);
+
   return (
     <div className="w-full">
-      <div className="h-[1000px]"></div>
-      <Footer />
+      <div className="h-[1000px]">
+        {posts.map((post) => {
+          return (
+            <div key={post.id}>
+              <News
+                title={post.title}
+                topic={post.topic}
+                image={post.image.url}
+                content={post.content.text}
+                time={post.time}
+                id={post.id}
+              />
+            </div>
+          );
+        })}
+      </div>
+      {/* <Footer /> */}
 
       <Scrollbackbutton />
     </div>
