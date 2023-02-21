9e261ffe0513c87ec4c46600d6adafe8381a8873
Mike <bsvusontung@gmail.com>
2023-02-21 15:03:18 +0700

    add: slider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,23 @@
 import React from "react";
-import Header from "../components/Header";
+// import Header from "../components/Header";
 import "../App.css";
 import Scrollbackbutton from "../components/Scrollbackbutton";
+import SliderAuto from "../javascripts/SliderAuto";
+import SliderManual from "../javascripts/SliderManual";
 
 function Home() {
   return (
     <div className="h-[40000px]">
       <Scrollbackbutton />
+      <div class="MustWatch grid grid-cols-10 h-300px">
+        <div className="mb-5 h-80% col-span-7">
+          <SliderAuto />
+        </div>
+        <div>
+          <h1 className="text-5xl text-white font-bold">Must Watch</h1>
+        </div>
+      </div>
+      <SliderManual />
     </div>
   );
 }
