586a866a1457f3d859d560335d9a7ad25f861934
Phong <pp072003@gmail.com>
2023-02-24 20:25:34 +0700

    Phong add ScrollTimeLine

---
+++ src/components/ScrollTimeline.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import HorizontalTimeline from "react-horizontal-timeline";
+import DemoKeyboard from "../javascripts/Timeline"
+
+const VALUES = ["  2001", "2000", "2009","2010","2022"];
+
+class App extends Component {
+  state = { value: 0, previous: 0 };
+
+  changeIndexFromSlide = index => {
+    this.setState({ value: index });
+  };
+  render() {
+    return (
+      <div>
+        {/* Bounding box for the Timeline */}
+        <div style={{ width: "100%", height: "100px", margin: "0 auto" }}>
+          <HorizontalTimeline
+            styles={{
+             
+              foreground: "red",
+              outline: "gray",
+              
+            }}
+            index={this.state.value}
+            indexClick={index => {
+              this.setState({ value: index, previous: this.state.value });
+            }}
+            values={VALUES}
+            
+          />
+        </div>
+        <div className="">
+          <DemoKeyboard
+            inputIndex={this.state.value}
+            outputIndex={this.changeIndexFromSlide}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
+
+export default App;
---
+++ src/javascripts/Timeline.js
@@ -0,0 +1,85 @@
+import React, { Component } from "react";
+import SwipeableViews from "react-swipeable-views";
+import { bindKeyboard } from "react-swipeable-views-utils";
+import "./style.css";
+const BindKeyboardSwipeableViews = bindKeyboard(SwipeableViews);
+
+class DemoKeyboard extends Component {
+  sendIndex = index => {
+    this.props.outputIndex(index);
+  };
+  render() {
+    return (
+      <BindKeyboardSwipeableViews
+        index={this.props.inputIndex}
+        onChangeIndex={index => {
+          this.sendIndex(index);
+        }}
+        enableMouseEvents
+      >
+        <div className="slide1">
+          <div className="h-72 space-x-2 flex border-2 border-slate-600 ">
+              <div className= "w-2/3 bg-blue-400">
+              <img className="h-72 w-full"src="https://i1-vnexpress.vnecdn.net/2023/02/22/1-jfif-1677053503-2557-1677057591.jpg?w=680&h=408&q=100&dpr=1&fit=crop&s=ilPqV3m9XMp7XRSHf-pWFg" alt="" />
+              
+              </div>
+              <div className= "w-1/3 bg-slate-100">
+                <h1 className="font-bold"> Nga lộ dấu hiệu hụt hơi trong chiến dịch tấn công mới ở Ukraine</h1>
+                <p>Chiến dịch tấn công mới của Nga ở miền đông Ukraine bộc lộ những hạn chế về chiến thuật và chất lượng huấn luyện, khiến Moskva khó tạo đột phá. </p>
+              </div>
+          </div>
+        </div>
+        <div className="slide2">
+        <div className="h-72 space-x-2 flex border-2 border-slate-600 ">
+              <div className= "w-2/3 bg-blue-400">
+              <img className="h-72 w-full"src="https://i1-vnexpress.vnecdn.net/2023/02/22/1-jfif-1677053503-2557-1677057591.jpg?w=680&h=408&q=100&dpr=1&fit=crop&s=ilPqV3m9XMp7XRSHf-pWFg" alt="" />
+              
+              </div>
+              <div className= "w-1/3 bg-slate-100">
+                <h1 className="font-bold"> Nga lộ dấu hiệu hụt hơi trong chiến dịch tấn công mới ở Ukraine</h1>
+                <p>Chiến dịch tấn công mới của Nga ở miền đông Ukraine bộc lộ những hạn chế về chiến thuật và chất lượng huấn luyện, khiến Moskva khó tạo đột phá. </p>
+              </div>
+          </div>
+        </div>
+        <div className="slide3">
+        <div className="h-72 space-x-2 flex border-2 border-slate-600 ">
+              <div className= "w-2/3 bg-blue-400">
+              <img className="h-72 w-full"src="https://i1-vnexpress.vnecdn.net/2023/02/22/1-jfif-1677053503-2557-1677057591.jpg?w=680&h=408&q=100&dpr=1&fit=crop&s=ilPqV3m9XMp7XRSHf-pWFg" alt="" />
+              
+              </div>
+              <div className= "w-1/3 bg-slate-100">
+                <h1 className="font-bold"> Nga lộ dấu hiệu hụt hơi trong chiến dịch tấn công mới ở Ukraine</h1>
+                <p>Chiến dịch tấn công mới của Nga ở miền đông Ukraine bộc lộ những hạn chế về chiến thuật và chất lượng huấn luyện, khiến Moskva khó tạo đột phá. </p>
+              </div>
+          </div>
+        </div>
+        <div className="slide4">
+        <div className="h-72 space-x-2 flex border-2 border-slate-600 ">
+              <div className= "w-2/3 bg-blue-400">
+              <img className="h-72 w-full"src="https://i1-vnexpress.vnecdn.net/2023/02/22/1-jfif-1677053503-2557-1677057591.jpg?w=680&h=408&q=100&dpr=1&fit=crop&s=ilPqV3m9XMp7XRSHf-pWFg" alt="" />
+              
+              </div>
+              <div className= "w-1/3 bg-slate-100">
+                <h1 className="font-bold"> Nga lộ dấu hiệu hụt hơi trong chiến dịch tấn công mới ở Ukraine</h1>
+                <p>Chiến dịch tấn công mới của Nga ở miền đông Ukraine bộc lộ những hạn chế về chiến thuật và chất lượng huấn luyện, khiến Moskva khó tạo đột phá. </p>
+              </div>
+          </div>
+        </div>
+        <div className="slide5">
+        <div className="h-72 space-x-2 flex border-2 border-slate-600 ">
+              <div className= "w-2/3 bg-blue-400">
+              <img className="h-72 w-full"src="https://i1-vnexpress.vnecdn.net/2023/02/22/1-jfif-1677053503-2557-1677057591.jpg?w=680&h=408&q=100&dpr=1&fit=crop&s=ilPqV3m9XMp7XRSHf-pWFg" alt="" />
+              
+              </div>
+              <div className= "w-1/3 bg-slate-100">
+                <h1 className="font-bold"> Nga lộ dấu hiệu hụt hơi trong chiến dịch tấn công mới ở Ukraine</h1>
+                <p>Chiến dịch tấn công mới của Nga ở miền đông Ukraine bộc lộ những hạn chế về chiến thuật và chất lượng huấn luyện, khiến Moskva khó tạo đột phá. </p>
+              </div>
+          </div>
+        </div>
+       </BindKeyboardSwipeableViews>
+    );
+  }
+}
+
+export default DemoKeyboard;
