e2a1941e7a86eea1b74364fe1f326b8335144e5d
Phong <pp072003@gmail.com>
2023-03-06 09:38:54 +0700

    Phong Error404

---
+++ src/pages/Error404.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "../javascripts/style.css";
+
+export default function Error404() {
+  return (
+    <div
+      style={{
+        backgroundImage: `url(https://colibriwp.com/blog/wp-content/uploads/2019/07/2488756.jpg)`,
+      }}
+      className="h-screen w-full bg-center bg-cover"
+    ></div>
+  );
+}
