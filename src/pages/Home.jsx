528af2d4dd71ee1c15621d9628af861984f4da89
tranvankhuyen <khuyendvdtht@gmail.com>
2023-03-07 09:43:08 +0700

    multi language

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,12 +8,14 @@ import News from "../components/News";
 import Rankingboard from "../components/Rankingboard";
 import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
 import ReactPaginate from "react-paginate";
-function Home() {
-  const [posts, setPosts] = useState([]);
+import useQueryPost from "../hook/useQueryPost";
 
-  useEffect(() => {
-    getPosts().then((newPosts) => setPosts(newPosts));
-  }, []);
+function Home() {
+  const { posts, error } = useQueryPost({ func: getPosts });
+
+  const POSTS = posts.filter((post) => post.language === "JP");
+  const postsVN = posts.filter((post) => post.language === "VN");
+
   const [currentPage, setCurrentPage] = useState(1);
   const [postsPerPage] = useState(9);
 
@@ -26,7 +28,7 @@ function Home() {
   const paginate = ({ selected }) => {
     setCurrentPage(selected + 1);
   };
-  return !posts.length ? (
+  return !POSTS.length ? (
     <div className="flex h-screen w-screen items-center justify-center"></div>
   ) : (
     <div className="z-10 w-full pb-10">
@@ -40,18 +42,18 @@ function Home() {
             <div class="relative ">
               <img
                 className="h-[400px] w-full  border border-slate-700"
-                src={posts[5].image.url}
+                src={POSTS[5].image.url}
               />
               <div className="bg-gradient absolute bottom-0 w-full p-4">
                 <h5 className="mb:text-3xl overflow-hidden text-ellipsis break-words px-2 pb-8 text-5xl font-bold text-gray-100 md:pb-5 lg:pb-3 lg:text-xl lg:font-semibold">
-                  <a href="">{posts[5].title}</a>
+                  <a href="">{POSTS[5].title}</a>
                 </h5>
                 <div className="flex items-center space-x-3 px-2">
                   <div className="md:text-md text-xl font-light leading-none text-white lg:text-xs">
-                    {posts[5].topic}
+                    {POSTS[5].topic}
                   </div>
                   <span className="md:text-md text-lg font-light text-gray-200 lg:text-xs">
-                    {posts[5].time}
+                    {POSTS[5].time}
                   </span>
                 </div>
               </div>
@@ -65,79 +67,79 @@ function Home() {
           <h1 className="font-bold">Đáng chú ý</h1>
           <div className="flex">
             <a
-              href={`detail/${posts[10].slug}`}
+              href={`detail/${POSTS[5].slug}`}
               className="w-[45%] py-4 pr-4 min-[300px]:hidden lg:block"
             >
-              <img src={posts[10].image.url} alt="" />
+              <img src={POSTS[5].image.url} alt="" />
               <div className="py-2 ">
-                <h3 className=" text-md font-bold">{posts[10].title}</h3>
+                <h3 className=" text-md font-bold">{POSTS[5].title}</h3>
                 <div className="flex py-1">
-                  <p className="px-1 text-sm">{posts[10].topic}</p>
-                  <p className="px-1 text-sm"> -{posts[10].time}</p>
+                  <p className="px-1 text-sm">{POSTS[5].topic}</p>
+                  <p className="px-1 text-sm"> -{POSTS[5].time}</p>
                 </div>
               </div>
             </a>
             <div className="flex h-[400px] flex-col py-4 px-2 min-[300px]:w-full lg:w-[55%]">
               <a
-                href={`detail/${posts[11].slug}`}
+                href={`detail/${POSTS[5].slug}`}
                 className="min-h-1/3 h-1/3 w-full pb-2 min-[300px]:flex "
               >
                 <img
                   className="min-[300px]:h-[100px] lg:h-[70px]"
-                  src={posts[11].image.url}
+                  src={POSTS[5].image.url}
                   alt=""
                 />
                 <div className="px-2">
                   <h3 className=" min-[300px]:text-md font-bold lg:text-sm">
-                    {posts[11].title}
+                    {POSTS[5].title}
                   </h3>
                   <div className="flex py-1">
                     <p className="min-[300px]:text-md px-1 lg:text-sm">
-                      {posts[11].topic}
+                      {POSTS[5].topic}
                     </p>
-                    <p className="px-1 text-sm"> -{posts[11].time}</p>
+                    <p className="px-1 text-sm"> -{POSTS[5].time}</p>
                   </div>
                 </div>
               </a>
               <a
-                href={`detail/${posts[12].slug}`}
+                href={`detail/${POSTS[5].slug}`}
                 className="min-h-1/3 h-1/3 w-full pb-2 min-[300px]:flex "
               >
                 <img
                   className="min-[300px]:h-[100px] lg:h-[70px]"
-                  src={posts[12].image.url}
+                  src={POSTS[5].image.url}
                   alt=""
                 />
                 <div className="px-2">
                   <h3 className=" min-[300px]:text-md font-bold lg:text-sm">
-                    {posts[12].title}
+                    {POSTS[5].title}
                   </h3>
                   <div className="flex py-1">
                     <p className="min-[300px]:text-md px-1 lg:text-sm">
-                      {posts[12].topic}
+                      {POSTS[5].topic}
                     </p>
-                    <p className="px-1 text-sm"> -{posts[12].time}</p>
+                    <p className="px-1 text-sm"> -{POSTS[5].time}</p>
                   </div>
                 </div>
               </a>
               <a
-                href={`detail/${posts[17].slug}`}
+                href={`detail/${POSTS[5].slug}`}
                 className="min-h-1/3 h-1/3 w-full pb-2 min-[300px]:flex "
               >
                 <img
                   className="min-[300px]:h-[100px] lg:h-[70px]"
-                  src={posts[17].image.url}
+                  src={POSTS[5].image.url}
                   alt=""
                 />
                 <div className="px-2">
                   <h3 className=" min-[300px]:text-md font-bold lg:text-sm">
-                    {posts[17].title}
+                    {POSTS[5].title}
                   </h3>
                   <div className="flex py-1">
                     <p className="min-[300px]:text-md px-1 lg:text-sm">
-                      {posts[17].topic}
+                      {POSTS[5].topic}
                     </p>
-                    <p className="px-1 text-sm"> -{posts[17].time}</p>
+                    <p className="px-1 text-sm"> -{POSTS[5].time}</p>
                   </div>
                 </div>
               </a>
@@ -147,41 +149,41 @@ function Home() {
         <div className="flex h-[400px] w-1/3 flex-col px-4 min-[300px]:hidden lg:flex">
           <h1 className="font-bold">Trending</h1>
           <a
-            href={`detail/${posts[14].slug}`}
+            href={`detail/${POSTS[5].slug}`}
             className="min-h-1/3 relative flex h-1/3 w-full py-4 pb-2 "
           >
-            <img className="h-[70px]" src={posts[14].image.url} alt="" />
+            <img className="h-[70px]" src={POSTS[5].image.url} alt="" />
             <div className="px-2">
-              <h3 className=" text-sm font-bold">{posts[14].title}</h3>
+              <h3 className=" text-sm font-bold">{POSTS[5].title}</h3>
               <div className="flex py-1">
-                <p className="px-1 text-sm">{posts[14].topic}</p>
-                <p className="px-1 text-sm"> -{posts[14].time}</p>
+                <p className="px-1 text-sm">{POSTS[5].topic}</p>
+                <p className="px-1 text-sm"> -{POSTS[5].time}</p>
               </div>
             </div>
           </a>
           <a
-            href={`detail/${posts[15].slug}`}
+            href={`detail/${POSTS[5].slug}`}
             className="min-h-1/3 relative flex h-1/3 w-full pb-2 "
           >
-            <img className="h-[70px]" src={posts[15].image.url} alt="" />
+            <img className="h-[70px]" src={POSTS[5].image.url} alt="" />
             <div className="px-2">
-              <h3 className=" text-sm font-bold">{posts[15].title}</h3>
+              <h3 className=" text-sm font-bold">{POSTS[5].title}</h3>
               <div className="flex py-1">
-                <p className="px-1 text-sm">{posts[15].topic}</p>
-                <p className="px-1 text-sm"> -{posts[15].time}</p>
+                <p className="px-1 text-sm">{POSTS[5].topic}</p>
+                <p className="px-1 text-sm"> -{POSTS[5].time}</p>
               </div>
             </div>
           </a>
           <a
-            href={`detail/${posts[16].slug}`}
+            href={`detail/${POSTS[5].slug}`}
             className="min-h-1/3 relative flex h-1/3 w-full pb-2 "
           >
-            <img className="h-[70px]" src={posts[16].image.url} alt="" />
+            <img className="h-[70px]" src={POSTS[5].image.url} alt="" />
             <div className="px-2">
-              <h3 className=" text-sm font-bold">{posts[16].title}</h3>
+              <h3 className=" text-sm font-bold">{POSTS[5].title}</h3>
               <div className="flex py-1">
-                <p className="px-1 text-sm">{posts[16].topic}</p>
-                <p className="px-1 text-sm"> -{posts[16].time}</p>
+                <p className="px-1 text-sm">{POSTS[5].topic}</p>
+                <p className="px-1 text-sm"> -{POSTS[5].time}</p>
               </div>
             </div>
           </a>
@@ -206,7 +208,7 @@ function Home() {
                 breakLabel={<div className="mr-3"> . . . </div>}
                 pageRangeDisplayed={3}
                 onPageChange={paginate}
-                pageCount={Math.ceil(posts.length / postsPerPage)}
+                pageCount={Math.ceil(POSTS.length / postsPerPage)}
                 previousLabel={
                   <div className="mr-3 flex h-8 w-8 items-center justify-center bg-gray-400">
                     <BsChevronLeft className="flex items-center justify-center text-gray-600" />
